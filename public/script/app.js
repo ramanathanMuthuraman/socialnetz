4031ce17e506baceac6e4c9382f4f14cba3d4ec8
ramanathanMuthuraman <ramanathan.gksvv@gmail.com>
2014-10-22 22:31:20 +0530

    initial message

--- public/script/app.js
+++ public/script/app.js
@@ -10,7 +10,36 @@
     // device APIs are available
     //
     function onDeviceReady() {
-      
+        window.fbAsyncInit = function() {
+        FB.init({
+          appId      : NETWORK.FACEBOOK_CLIENT_ID,
+          xfbml      : true,
+          version    : 'v2.1'
+        });
+         
+         FB.login(function(response) {
+   if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       alert('Good to see you, ' + response.name + '.');
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ });
+         
+         
+        
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+    
     }
 
 var NETWORK = {
@@ -207,36 +236,7 @@ var context = {
 
 $(document).ready(function(){
     
-     window.fbAsyncInit = function() {
-        FB.init({
-          appId      : NETWORK.FACEBOOK_CLIENT_ID,
-          xfbml      : true,
-          version    : 'v2.1'
-        });
-         
-         FB.login(function(response) {
-   if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       alert('Good to see you, ' + response.name + '.');
-     });
-   } else {
-     console.log('User cancelled login or did not fully authorize.');
-   }
- });
-         
-         
-        
-      };
-
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-    
+   
     
   // $.socialnetz();
  
