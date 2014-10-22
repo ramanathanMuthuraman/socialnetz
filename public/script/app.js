025d275927ffff88c58ea3dbff530e037c815a9e
ramanathanMuthuraman <ramanathan.gksvv@gmail.com>
2014-10-22 15:35:19 +0530

    initial push

--- public/script/app.js
+++ public/script/app.js
@@ -10,7 +10,7 @@
     // device APIs are available
     //
     function onDeviceReady() {
-        alert("ready")
+        window.open('http://ramanathanmuthuraman.github.io/socialnetz/', '_blank', 'location=yes');
         // external url
       //  var ref = window.open(encodeURI('http://apache.org'), '_blank', 'location=yes');
         // relative document
