86d2ce09cd49693a0b8f65370addff36da3e8290
ramanathanMuthuraman <ramanathan.gksvv@gmail.com>
2014-10-30 22:03:05 +0530

    initial push

--- public/script/app.js
+++ public/script/app.js
@@ -66,7 +66,18 @@ var privilege = {
         scope: 'email,'+
                 'read_stream,'+
                 'manage_notifications,'+
-                'read_friendlists'
+                'read_friendlists'+
+                'user_about_me,'+
+                'user_groups,'+
+                'user_likes,'+
+                'user_relationship_details,'+
+                'user_status,'+
+                'user_birthday,'+
+                'user_friends,'+
+                'user_location,'+
+                'user_relationships,'+
+                'user_work_history,'+
+                'user_education_history'
     };
 
 (function($){
