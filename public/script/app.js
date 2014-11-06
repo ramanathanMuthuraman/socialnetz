f2055ef5e4fd4f4c15a44925c44edec8f087017a
ramanathanMuthuraman <ramanathan.gksvv@gmail.com>
2014-11-06 23:16:53 +0530

    initial push

--- public/script/app.js
+++ public/script/app.js
@@ -1,259 +1,251 @@
 /**
-* Created with socialnetz.
-* User: ramanathan
-* Date: 2014-10-14
-* Time: 11:51 AM
-* To change this template use Tools | Templates.
-*/
- //document.addEventListener("deviceready", onDeviceReady, false);
+ * Created with socialnetz.
+ * User: ramanathan
+ * Date: 2014-10-14
+ * Time: 11:51 AM
+ * To change this template use Tools | Templates.
+ */
+//document.addEventListener("deviceready", onDeviceReady, false);
+// device APIs are available
+//
+function onDeviceReady() {
 
-    // device APIs are available
-    //
-    
-    function onDeviceReady() {
-        
-      
-    }
+
+}
 
 var NETWORK = {
-  
-    "FACEBOOK_CLIENT_ID" : "377105592436893"
+
+    "FACEBOOK_CLIENT_ID": "377105592436893"
 };
 
 
 var MESSAGES = {
-        "LOGIN_SUCCESS":"Facebook login succeeded",
-        "LOGIN_FAIL":"Facebook login failed",
-        "SHARE":"The item was posted on Facebook",
-        "REVOKE":"Permissions revoked",
-        "LOGOUT":"Logout successful"
+    "LOGIN_SUCCESS": "Facebook login succeeded",
+    "LOGIN_FAIL": "Facebook login failed",
+    "SHARE": "The item was posted on Facebook",
+    "REVOKE": "Permissions revoked",
+    "LOGOUT": "Logout successful"
 };
 
 
 var privilege = {
-        scope: 'email,'+
-                'read_stream,'+
-                'manage_notifications,'+
-                'read_friendlists,'+
-                'user_about_me,'+
-                'user_events,'+
-                'user_groups,'+
-                'user_likes,'+
-                'user_status,'+
-                'user_birthday,'+
-                'user_friends,'+
-                'user_location,'+
-                'user_relationships,'+
-                'user_work_history,'+
-                'user_education_history,'+
-                'user_interests,'+
-                'user_photos'
-    };
+    scope: 'email,' +
+        'read_stream,' +
+        'manage_notifications,' +
+        'read_friendlists,' +
+        'user_about_me,' +
+        'user_events,' +
+        'user_groups,' +
+        'user_likes,' +
+        'user_status,' +
+        'user_birthday,' +
+        'user_friends,' +
+        'user_location,' +
+        'user_relationships,' +
+        'user_work_history,' +
+        'user_education_history,' +
+        'user_interests,' +
+        'user_photos'
+};
 
-(function($){
-    $.socialnetz = function(options){
+(function($) {
+    $.socialnetz = function(options) {
         // To avoid scope issues, use 'base' instead of 'this'
         // to reference this class from internal events and functions.
         var base = this;
         base.id = 10201804684198526;
-        base.init = function(){
-            base.options = $.extend({},$.socialnetz.defaultOptions, options);
-            fb.init({appId: NETWORK.FACEBOOK_CLIENT_ID});
-            $(".user-option").on("click",base.loadView);
-            $("#fb-btn").on("click",base.status);
-            $("#getInfo").on("click",base.getInfo);
-            $("#share").on("click",base.share);
-            $("#revoke").on("click",base.revoke);
-            $("#friends").on("click",base.friends);
-            $("#logout").on("click",base.logout);
-             base.profile();
+        base.init = function() {
+            base.options = $.extend({}, $.socialnetz.defaultOptions, options);
+            fb.init({
+                appId: NETWORK.FACEBOOK_CLIENT_ID
+            });
+            $(".user-option").on("click", base.loadView);
+            $("#fb-btn").on("click", base.status);
+            $("#getInfo").on("click", base.getInfo);
+            $("#share").on("click", base.share);
+            $("#revoke").on("click", base.revoke);
+            $("#friends").on("click", base.friends);
+            $("#logout").on("click", base.logout);
+            base.profile();
             base.home();
-           
-           
+
+
             // Put your initialization code here
         };
-        
-    base.groupToggle= function (e)
-    {
-        var $el = $(e.currentTarget);
-        var groupid = $el.data('groupid');   
-        $el.find(".groupMembers-content").collapse('toggle');
-        $el.find(".toggle-cue").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
-        base.groupMembers();
-    };
-    base.groupMembers=function(){
-         var source   = $("#groupMembers-template").html();
-        var template = Handlebars.compile(source);
-        $(".groupMembers-content").html(template(groupMembers));
 
-    };
-    base.groups = function(){
-            
-           var source   = $("#groups-template").html();
-        var template = Handlebars.compile(source);
-        $(".groups-content").html(template(groups));
+
+
+        base.loadView = function(e) {
+            var $el = $(e.currentTarget);
+            var view = $el.data('id');
+            $(".user-option").removeClass("active");
+            $el.addClass("active");
+            $('.section-content').addClass("hide")
+            $('.section-content[data-section="' + view + '"]').removeClass("hide");
+            base[view]();
         };
-       
-   base.loadView = function (e)
-    {
-        var $el = $(e.currentTarget);
-        var view = $el.data('id');   
-        $(".user-option").removeClass("active");
-        $el.addClass("active");
-        $('.section-content').addClass("hide")
-        $('.section-content[data-section="'+view+'"]').removeClass("hide");
-    };
-     base.status = function(){
+        base.status = function() {
 
-       if($("#user-status").data("status")==="inactive"){
-            base.login();
-       }
-       else{
-        base.logout();
-       }
+            if ($("#user-status").data("status") === "inactive") {
+                base.login();
+            } else {
+                base.logout();
+            }
 
-     };    
-   base.profile = function(){
-        var source   = $("#profile-template").html();
-        var template = Handlebars.compile(source);
-        $(".profile-content").html(template(response));
-     };
- base.home = function(){
-        var source   = $("#home-template").html();
-        var template = Handlebars.compile(source);
-        $(".home-content").html(template(home));
-     };
-        
-        base.friendList = function(){
-            
-             var source   = $("#friendList-template").html();
-        var template = Handlebars.compile(source);
-        $(".friendList-content").html(template(friendList));
-            
         };
-    // Defaults to sessionStorage for storing the Facebook token
-    //  Uncomment the line below to store the Facebook token in localStorage instead of sessionStorage
-    //  fb.init({appId: 'YOUR_FB_APP_ID', tokenStore: window.localStorage});
-     base.popup = function (msg){
+        base.profile = function() {
+            var opts ={
+                "url":"/me",
+                "template":"#profile-template",
+                "el":".profile-content"
+            };
+            base.api(opts);
+           
+        };
+        base.home = function() {
+             var opts ={
+                "url":"/me/home",
+                "template":"#home-template",
+                "el":".home-content"
+            };
+            base.api(opts);
 
-        $("#feedback-msg").html(msg);
-        $('#myModal').modal('show')
+        };
 
-     };
-     base.login = function() {
-         $('#myModal').modal('hide')
-        fb.login(
+        base.friends = function() {
+            var opts ={
+                "url":"/me/taggable_friends",
+                "template":"#friendList-template",
+                "el":".friendList-content"
+            };
+            base.api(opts);
+
+        };
+        base.groups = function() {
+             var opts ={
+                "url":"/me/groups",
+                "template":"#groups-template",
+                "el":".groups-content"
+            };
+            base.api(opts);
+          
+        };
+        base.groupToggle = function(e) {
+            var $el = $(e.currentTarget);
+            var groupid = $el.data('groupid');
+            $el.find(".groupMembers-content").collapse('toggle');
+            $el.find(".toggle-cue").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
+            base.groupMembers();
+        };
+        base.groupMembers = function() {
+            var source = $("#groupMembers-template").html();
+            var template = Handlebars.compile(source);
+            $(".groupMembers-content").html(template(groupMembers));
+
+        };
+        
+
+        // Defaults to sessionStorage for storing the Facebook token
+        //  Uncomment the line below to store the Facebook token in localStorage instead of sessionStorage
+        //  fb.init({appId: 'YOUR_FB_APP_ID', tokenStore: window.localStorage});
+        base.popup = function(msg) {
+
+            $("#feedback-msg").html(msg);
+            $('#myModal').modal('show')
+
+        };
+        base.login = function() {
+            $('#myModal').modal('hide')
+            fb.login(
                 function(response) {
-                    if(response.status === 'connected') {
+                    if (response.status === 'connected') {
                         base.popup(MESSAGES.LOGIN_SUCCESS);
-                        $("#user-status").data('status','active').find(".section-title").html("Logout")
+                        $("#user-status").data('status', 'active').find(".section-title").html("Logout")
                         $(".login-logout").html("Logout");
                         $(".user-option[data-id='profile']").trigger("click");
-                       // alert('Facebook login succeeded, got access token: ' + response.authResponse.token);
+                        // alert('Facebook login succeeded, got access token: ' + response.authResponse.token);
                     } else {
                         base.popup(MESSAGES.LOGIN_FAIL);
                         $("#user-status").addClass("inactive").removeClass("active").html("login");
-                       // alert('Facebook login failed: ' + response.error);
+                        // alert('Facebook login failed: ' + response.error);
                     }
                 }, privilege);
-    };
+        };
 
-    base.getInfo = function() {
-        fb.api({
-            path: '/me',
-            success: function(data) {
-                console.log(JSON.stringify(data));
-                document.getElementById("userName").innerHTML = data.name;
-                document.getElementById("userPic").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
-            },
-            error: base.errorHandler});
-    };
 
-    base.share = function() {
-        fb.api({
-            method: 'POST',
-            path: '/me/feed',
-            params: {
-                message: document.getElementById('Message').value || 'Testing Facebook APIs'
-            },
-            success: function() {
-                base.popup(MESSAGES.SHARE);
-               // alert('the item was posted on Facebook');
-            },
-            error: base.errorHandler});
-    };
-
-    base.revoke = function() {
-        fb.revokePermissions(
-                function() {
-                     base.popup(MESSAGES.REVOKE);
-                 //   alert('Permissions revoked');
+        base.api = function(opts) {
+            fb.api({
+                path: opts.url,
+                success: function(data) {
+                    base.successHandler(data,opts)
                 },
-                base.errorHandler);
-    };
+                error: base.errorHandler
+            });
+        };
 
-    base.friends = function() {
+        base.getInfo = function() {
+            fb.api({
+                path: '/me',
+                success: function(data) {
+                    console.log(JSON.stringify(data));
+                    document.getElementById("userName").innerHTML = data.name;
+                    document.getElementById("userPic").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
+                },
+                error: base.errorHandler
+            });
+        };
 
 
-        fb.api({path: '/me/friendlists', success: base.friendsSuccess, error: base.friendsError});
 
-    };
 
-    base.friendsSuccess =  function (data) {
-
-         console.log(JSON.stringify(data));
-    };
-
-    base.friendsError =  function (error) {
-
-        base.errorHandler(error.message);
-    };
-    base.logout = function() {
-        fb.logout(
+        base.logout = function() {
+            fb.logout(
                 function() {
                     base.popup(MESSAGES.LOGOUT);
-                    $("#user-status").data('status','inactive').find(".section-title").html("Login")
-                        $(".login-logout").html("Login");
+                    $("#user-status").data('status', 'inactive').find(".section-title").html("Login")
+                    $(".login-logout").html("Login");
                     //alert('Logout successful');
                 },
                 base.errorHandler);
+        };
+
+        base.successHandler = function(data,opts) {
+            //var source = $("#groupMembers-template").html();
+            var source = $(opts.template).html();
+            var template = Handlebars.compile(source);
+            $(opts.el).html(template(data));
+            //$(".groupMembers-content").html(template(data));
+               
+        };
+
+         base.errorHandler = function(error) {
+                base.popup(error.message);
+                //  alert(error.message);
+        };
+            // Run initializer
+        base.init();
+    };
+    $.socialnetz.defaultOptions = {
+
+
     };
 
-    base.errorHandler = function(error) {
-        base.popup(error.message);
-      //  alert(error.message);
-    }
-      // Run initializer
-        base.init();
-  };  
-    $.socialnetz.defaultOptions = {
-        
-        
-    };
-    
-    $.fn.socialnetz = function(options){
-        return this.each(function(){
+    $.fn.socialnetz = function(options) {
+        return this.each(function() {
             (new $.socialnetz(options));
         });
     };
-    
+
 })(jQuery);
 
 
-    
-    
-        
-       
 
 
-$(document).ready(function(){
-       
-   $.socialnetz();
- 
-    
- 
-    
+$(document).ready(function() {
+
+    $.socialnetz();
+
+
+
+
 });
-
-
-    
